1fd3b736abfed55587d67cb7d8614106fa45f9f8
unknown <becherers7@gmail.com>
2020-06-28 11:45:50 -0500

    Test.

--- App.js
+++ App.js
@@ -1,19 +1,57 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {
+  Alert,
+  Button,
+  Image,
+  StyleSheet,
+  Text,
+  TouchableWithoutFeedback,
+  View,
+  SafeAreaView
+} from "react-native";
 
 export default function App() {
+  const handlePress = text => {
+    console.log({ text });
+  };
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text onPress={() => handlePress("Text pressed.")}>
+        The Dark Ravens Will Fly.
+      </Text>
+      <TouchableWithoutFeedback onPress={() => handlePress("Image pressed.")}>
+        <Image
+          source={{
+            width: 200,
+            height: 300,
+            uri: "https://picsum.photos/200/300"
+          }}
+        />
+      </TouchableWithoutFeedback>
+      <Button
+        title="Click me"
+        onPress={() =>
+          Alert.alert("Title", "Message", [
+            {
+              text: "Yes",
+              onPress: () => handlePress("Selected yes")
+            },
+            {
+              text: "No",
+              onPress: () => handlePress("Selected no")
+            }
+          ])
+        }
+      ></Button>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    alignItems: "center"
+  }
 });
